e1e2c42cff304630d529ac9f8396ddc2045d837a
iqbalmkom33 <iqbal.mkom@gmail.com>
2024-12-22 17:45:52 +0700

    11

--- wp-content/themes/generatepress/assets/js/wq.js
+++ wp-content/themes/generatepress/assets/js/wq.js
@@ -1,18 +1,18 @@
 let curUrl = window.location.href;   
-console.log(curUrl);
+//console.log(curUrl);
 
 let wqreferer = document.referrer;
-console.log("referer="+wqreferer);
+//console.log("referer="+wqreferer);
 
 if(curUrl.includes("?wqqs=")){
     //console.log("Lanjut Refresh untuk hilangkan QS");
     let arrQs   = curUrl.split('?wqqs=');
     let linkArtikel = arrQs[0];
     
-    console.log(arrQs);
-    console.log(linkArtikel);
+    //console.log(arrQs);
+    //console.log(linkArtikel);
 
-    console.log("masuk atas");
+    //console.log("masuk atas");
     window.location.href = linkArtikel;
 }else{
     if(wqreferer != undefined){
@@ -25,9 +25,13 @@ if(curUrl.includes("?wqqs=")){
                                 //.split("wqunder").join("_")
                                 .split("wqequal").join("=");;
 
-            console.log("Setelah Redirect Dari QS ke linkuang="+linkUang);
+            //console.log("Setelah Redirect Dari QS ke linkuang="+linkUang);
             //alert("linkuang="+linkUang);
-            //window.location.href = linkUang;
-        }else{console.log("not wqqs");}
-    }else{console.log("not ref");}
+            window.location.href = linkUang;
+        }else{
+            //console.log("not wqqs");
+            }
+    }else{
+        //console.log("not ref");
+    }
 }
